ec3b8b159942b36764f4cc3d46759180a1657a7e
Javier Revillas <jrevillas@massivedynamic.io>
2016-03-26 17:18:11 +0100

    20160326-1718

---
+++ models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+  "github.com/satori/go.uuid"
+  "golang.org/x/crypto/bcrypt"
+  // "gopkg.in/mgo.v2"
+)
+
+type User struct {
+  ID       string `json:"id"`
+  Email    string `json:"email"`
+  Password string `json:"password"`
+  Username string `json:"username"`
+}
+
+func NewUser(email, password, username string) *User {
+  encrypted, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+  if err != nil {
+    panic(err)
+  }
+  user := &User{
+    ID:       uuid.NewV4().String(),
+    Email:    email,
+    Password: string(encrypted),
+    Username: username,
+  }
+  return user
+}
