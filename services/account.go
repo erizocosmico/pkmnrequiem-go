bbd2acbc1faaa6e74f6a4917d1a9970710b1ed3b
Javier Revillas <jrevillas@massivedynamic.io>
2016-05-02 18:15:31 +0200

    Empty comments.

--- services/account.go
+++ services/account.go
@@ -1,45 +1,51 @@
 package services
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "github.com/jrevillas/pkmnrequiem-go/middlewares"
-  "github.com/jrevillas/pkmnrequiem-go/models"
-  "gopkg.in/mgo.v2"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jrevillas/pkmnrequiem-go/middlewares"
+	"github.com/jrevillas/pkmnrequiem-go/models"
+	"gopkg.in/mgo.v2"
 )
 
+// AccountService ...
 type AccountService struct {
-  *middlewares.Session
-  collection *mgo.Collection
-  store *models.UserStore
+	*middlewares.Session
+	collection *mgo.Collection
+	store      *models.UserStore
 }
 
+// NewAccountService ...
 func NewAccountService(db *mgo.Database) *AccountService {
-  return &AccountService{
-    collection: db.C("account"),
-    Session:    middlewares.NewSession(db),
-    store:      &models.UserStore{db},
-  }
+	return &AccountService{
+		collection: db.C("account"),
+		Session:    middlewares.NewSession(db),
+		store:      &models.UserStore{db},
+	}
 }
 
+// Register ...
 func (a *AccountService) Register(r *gin.RouterGroup) {
-  group := r.Group("/account")
-  group.POST("/create", a.Guest, a.Create)
-  // group.POST("/login", a.Guest, a.Login)
-  // group.POST("/logout", a.Auth, a.Logout)
+	group := r.Group("/account")
+	group.POST("/create", a.Guest, a.Create)
+	// group.POST("/login", a.Guest, a.Login)
+	// group.POST("/logout", a.Auth, a.Logout)
 }
 
+// Create ...
 func (a *AccountService) Create(c *gin.Context) {
-  var form CreateAccountForm
-  if err := c.BindJSON(&form); err != nil {
-    c.AbortWithError(http.StatusBadRequest, err)
-    return
-  }
-  c.JSON(http.StatusOK, form)
+	var form CreateAccountForm
+	if err := c.BindJSON(&form); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	c.JSON(http.StatusOK, form)
 }
 
+// CreateAccountForm ...
 type CreateAccountForm struct {
-  Email    string `binding:"email,required" json:"email"`
-  Password string `binding:"min=8,required" json:"password"`
-  Username string `binding:"alphanum,required" json:"username"`
+	Email    string `binding:"email,required" json:"email"`
+	Password string `binding:"min=8,required" json:"password"`
+	Username string `binding:"alphanum,required" json:"username"`
 }
--- services/service.go
+++ services/service.go
@@ -2,14 +2,17 @@ package services
 
 import "github.com/gin-gonic/gin"
 
+// Service ...
 type Service interface {
-  Register(*gin.RouterGroup)
+	Register(*gin.RouterGroup)
 }
 
+// Services ...
 type Services []Service
 
+// Register ...
 func (s Services) Register(r *gin.RouterGroup) {
-  for _, service := range s {
-    service.Register(r)
-  }
+	for _, service := range s {
+		service.Register(r)
+	}
 }
