f039d76cb5d92dbf4587ac181ba90d9620cb292d
Javier Revillas <jrevillas@massivedynamic.io>
2016-05-27 19:33:04 -0400

    Better logging.

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jrevillas/pkmnrequiem-go/services"
+	"github.com/op/go-logging"
 	"gopkg.in/mgo.v2"
 )
 
 var (
-	bindAddress = os.Getenv("RUN_ADDRESS")
-	dbName      = os.Getenv("MONGO_DB")
-	dbURI       = os.Getenv("MONGO_URI")
+	bindAddress  = os.Getenv("RUN_ADDRESS")
+	dbName       = os.Getenv("MONGO_DB")
+	dbURI        = os.Getenv("MONGO_URI")
+	format       = logging.MustStringFormatter(`%{color}%{time:15:04:05.000} %{level:.4s} %{id:03x}%{color:reset} %{message}`)
+	log          = logging.MustGetLogger("logger")
+	logBackend   = logging.NewLogBackend(os.Stdin, "", 0)
+	logFormatter = logging.NewBackendFormatter(logBackend, format)
 )
 
 func main() {
+	logging.SetBackend(logFormatter)
+	log.Notice("pkmnRequiem 0.1")
+	log.Notice("Conectando con la base de datos...")
 	db, err := dbConnection()
 	if err != nil {
 		log.Fatal(err)
@@ -30,9 +38,11 @@ func main() {
 }
 
 func dbConnection() (*mgo.Database, error) {
+	start := time.Now()
 	session, err := mgo.Dial(dbURI)
 	if err != nil {
 		return nil, err
 	}
+	log.Noticef("Base de datos conectada (%.2fms)", time.Since(start).Seconds()*1000)
 	return session.DB(dbName), nil
 }
